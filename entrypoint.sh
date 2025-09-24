276b178cd0fea6564d07cbd4a5b90209e3cb02b4
LOKI-25 <plokeshn25@gmail.com>
2025-09-23 22:08:25 -0500

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # 1. Download the knowledge base from S3
 # Make sure to replace <your-bucket-name> with the actual name of your S3 bucket
 echo "Downloading knowledge base from S3..."
-aws s3 cp s3://ucm-chatbot-knowledge-base/ucmo_chroma_store.zip /app/ucmo_chroma_store.zip
+aws s3 cp s3://ucm-chatbot-kb/ucmo_chroma_store.zip /app/ucmo_chroma_store.zip
 
 # 2. Unzip the knowledge base
 echo "Unzipping knowledge base..."
